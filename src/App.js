613e6631b56cd06f808e9f1251a8d0fd18da8007
adloktionov <86207477+adloktionov@users.noreply.github.com>
2022-06-17 17:06:18 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,163 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center ">
+          <img width={40} hight={40} src="/svg.png" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин кросовок</p>
+          </div>
+        </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img width={18} hight={18} src="/cart.png" />
+            <span>1205 руб</span>
+          </li>
+          <li>
+            <img width={18} hight={18} src="/user.png" />
+          </li>
+        </ul>
+      </header>
+
+ <h1 class="p-40">Все кросовки</h1>
+
+      <div className="content p-40 d-flex flex-wrap align-center justify-between">
+
+
+      <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+       
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+        <div className="p-20 d-flex flex-column align-center" >
+          <img width={200} hight={200} src="/кросы3.png" />
+          <div>
+            <div>
+              <h5>Мужские кросовки Nike Fitness</h5>
+              <span>Цена:</span> <br/>
+              <b>12 999 рублей</b>
+            </div>
+            <button>купить</button>
+          </div>
+        </div>
+
+
+
+
+        
+
+
+      
+
+       
+        
+     
+
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
